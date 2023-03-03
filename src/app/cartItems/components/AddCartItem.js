ba9886ed2477185c7a22daa629ed8d53bb473744
Adrian Gąsiorek <adriangasiorek@interia.pl>
2023-03-03 03:02:24 +0100

    main page ui

--- src/app/cartItems/components/AddCartItem.js
+++ src/app/cartItems/components/AddCartItem.js
@@ -2,32 +2,19 @@ import React from "react"
 import { connect } from "react-redux"
 import actions from "../duck/actions"
 import { cartActions } from "../duck"
-import { useReducer } from "react"
 
 export const AddCartItem = (props) =>{
 
     const itemInput = React.createRef()
-    const [, forceUpdate] = useReducer(x => x + 1, 0);
 
   
     const addItem = (e) =>{
         const item = props.items.list.find((item) => item.title === (props.title))
-
         item ? 
             item.ammount = (item.ammount+1) 
             :
             props.add(props.title, props.price, props.thumbnail, 1)
-            
         props.update()        
-
-
-
-        // console.log(props.items.list)
-        // console.log(props.items)       
-        // console.log(item)
-        // console.log(props.items.list[0].title)
-        // props.items.list[0].title = 'chuj'
-        // props.add(props.title, props.price, props.thumbnail)
     }
 
     return( 
